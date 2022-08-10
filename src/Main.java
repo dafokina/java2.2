92787e2c3bcab10ed8eaa3dc34f2baf3c8c3fce1
Дарья Фокина <cool.russia-33@yandex.ru>
2022-08-10 20:58:31 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountOfMoney = 200;
+        int replenishmentAmount = 2000;
+
+        int percent;
+        if (replenishmentAmount > 1000) {
+            percent = replenishmentAmount / 100;
+        } else {
+            percent = 0;
+        }
+        System.out.println("Сумма ваших бонусов: " + percent);
+    }
+}
